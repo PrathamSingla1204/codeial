db931b6195b692bfc05f4830f3d5a2f9f6cca2ed
Pratham <Pratham.singla.762@gmail.com>
2023-05-19 15:18:24 +0530

    add controller and routes

---
+++ controllers/home_controller.js
@@ -0,0 +1,9 @@
+module.exports.home = function(req,res){
+    return res.end("<h1> EXpress is up for codeial</h1>")
+}
+
+module.exports.home1 = function(req,res){
+    return res.end("<h1> EXpress is down</h1>")
+}
+
+//module.exports.actionname = function(req,res){}
