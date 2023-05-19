f8ab9f83273e865929c1dca7c3e0199b06ac0ba0
Pratham <Pratham.singla.762@gmail.com>
2023-05-19 16:20:45 +0530

    add user controller and made index route as main route

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,5 @@
-module.exports.home = function(req,res){
-    return res.end("<h1> EXpress is up for codeial</h1>")
+module.exports.home = function(req, res){
+    return res.end('<h1>Express is up for Codeial!</h1>')
 }
 
-module.exports.home1 = function(req,res){
-    return res.end("<h1> EXpress is down</h1>")
-}
-
-//module.exports.actionname = function(req,res){}
+// module.exports.actionName = function(req, res){}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const router = express.Router();
 
+const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
-console.log("router working");
+console.log('router loaded');
 
-router.get('/',homeController.home1);
-//router.actionlena hai ('directory',homecontroller.actionname);
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+
+
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
+
 module.exports = router;
