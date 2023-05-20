bbceb1473fe4ca0ce09b2f1ff661ee75424448d4
Pratham <Pratham.singla.762@gmail.com>
2023-05-20 20:50:49 +0530

    Render pages for sigin and signup form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,16 @@ module.exports.profile = function(req, res){
         title : "user_profile"
     });
 }
+ 
+module.exports.signin = function(req,res){
+    return res.render('user_sign_in',{
+        title:"Sign-in"
+    });
+}
+
+
+module.exports.signup = function(req,res){
+    return res.render('user_sign_up',{
+        title:"Sign-up"
+    });
+}
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,7 @@ const usersConrtoller = require('../controllers/users_controller');
 
 router.get('/profile', usersConrtoller.profile);
 
+router.get('/signin', usersConrtoller.signin);
+router.get('/signup', usersConrtoller.signup);
+
 module.exports = router;
