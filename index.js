6fb899ec199248a54c4ca448f3c7898e5bf97de5
Pratham <Pratham.singla.762@gmail.com>
2023-05-25 02:26:24 +0530

    Add design to app using scss

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const MongoStore = require('connect-mongo');
 const sassMiddleware = require('express-dart-sass');
 
 app.use(sassMiddleware({
-    src : './assets/scss',
-    dest:'./assets/css',
-    debug:true,
-    outputStyle:'extended',
-    prefix:'./css'
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'expanded',
+    prefix: '/css',
 }));
 app.use(express.urlencoded());
 
