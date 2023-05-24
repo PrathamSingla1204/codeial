1978f47a20de2d579ce87d694cd9dcc80142439a
Pratham <Pratham.singla.762@gmail.com>
2023-05-24 20:33:24 +0530

    signin and signup setup using passport js

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,10 +1,12 @@
 const passport = require('passport');
 
 const LocalStrategy = require('passport-local').Strategy;
- 
+
 const User = require('../models/user');
 
-//authentication using passport
+
+// authentication using passport
+
 passport.use(new LocalStrategy({
     usernameField: 'email'
 }, async function(email, password, done) {
@@ -23,13 +25,14 @@ passport.use(new LocalStrategy({
 }));
 
 
-//Serializing user to decide which key is to be kept in the cookies
-passport.serializeUser((user,done)=>{
-    done(null,user.id);
-})
+// serializing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
 
 
-//deSerializing user from key  in the cookies
+
+// deserializing the user from the key in the cookies
 passport.deserializeUser(async (id, done) => {
     try {
         const user = await User.findById(id);
@@ -40,5 +43,31 @@ passport.deserializeUser(async (id, done) => {
     }
 });
 
+
+
+
+//check if the user is authenticated
+// check if the user is authenticated
+passport.checkAuthentication = function(req, res, next)
+{
+    // if the user is signed in, then pass on the request to the next function(controller's action)
+    if(req.isAuthenticated())
+    {
+        return next();
+    }
+
+    // if the user is not signed in
+    return res.redirect('/users/sign-in');
+}
+
+passport.setAuthenticatedUser = function(req, res, next)
+{
+    if(req.isAuthenticated())
+    {
+        // req.user contains the current signed in user from the session cookie and we are just sending it to the local for the views
+        res.locals.user = req.user;
+    }
+    next();
+}
 
 module.exports = passport;
