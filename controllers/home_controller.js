d7fdc1b1f4bd15a8f5039ed2f6fcd315796404fe
Pratham <Pratham.singla.762@gmail.com>
2023-05-26 18:17:39 +0530

    Shown comment on home screen using nested populate

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,15 @@ const Post = require('../models/post');
 module.exports.home = async function(req, res) {
   
     try {
-      let posts = await Post.find({}).populate('user').exec();
+      let posts = await Post.find({})
+      .populate('user')
+      .populate({
+        path: 'comments',
+        populate: {
+          path:'user'
+        }
+      })
+      .exec();
       return res.render('home', {
         title: "Codeial | Home",
         posts: posts
