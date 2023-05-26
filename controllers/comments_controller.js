9dd036eb25082f7de9553f427504ac449c1e2089
Pratham <Pratham.singla.762@gmail.com>
2023-05-26 20:55:01 +0530

    added functionalties to delete comment and made partails for comments & posts

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -19,3 +19,36 @@ module.exports.create = async function(req, res) {
     }
   }
   
+
+//   module.exports.destroy = function(req,res){
+
+//     Comment.findById(req.params.id,function(err,comment){
+//         if(comment.user == req.user.id){
+//             let postId = comment.post;
+
+//             comment.remove();
+//             Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},function(err,post){
+//                 return redirect('back');
+//             });
+//         }else{
+//             return redirect('back');
+//         }
+//     });
+//   }
+
+
+module.exports.destroy = async function(req, res) {
+    try {
+      const comment = await Comment.findById(req.params.id);
+      if (comment.user == req.user.id) {
+        let postId = comment.post;
+        await Comment.deleteOne({ _id: req.params.id });
+        await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } });
+        return res.redirect('back');
+      } else {
+        return res.redirect('back');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
