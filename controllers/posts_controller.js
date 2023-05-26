60ce19db7d90fbac14a093fa2373cf8e52db323f
Pratham <Pratham.singla.762@gmail.com>
2023-05-26 19:59:57 +0530

    added delete post functionality which also delete comments on that post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 module.exports.create = async function(req, res) {
     try {
       await Post.create({
@@ -11,3 +12,32 @@ module.exports.create = async function(req, res) {
     return res.redirect('back');
   }
   
+// module.exports.destroy = function(req,res){
+//   Post.findById(req.params.id,function(err,post){
+// //.id means converting the object id into string
+//     if(post.user == req.user.id){
+//       post.remove();
+//       Comment.deleteMany({post:req.params.id},function(err){
+//         return res.redirect('back');
+//       });
+//     }
+//     else{
+//       return res.redirect('back');
+//     }
+//   });
+// }
+
+module.exports.destroy = async function(req, res) {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post && post.user == req.user.id) {
+      await Post.deleteOne({ _id: req.params.id });
+      await Comment.deleteMany({ post: req.params.id });
+      return res.redirect('back');
+    } else {
+      return res.redirect('back');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
