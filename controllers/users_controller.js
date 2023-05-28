d287a4b60f5b9a29b8530da1036229b3f189a27f
Pratham <Pratham.singla.762@gmail.com>
2023-05-28 23:14:23 +0530

    Add Update profile features and some more styles to home page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -23,6 +23,19 @@ module.exports.profile = async function(req, res) {
     }
   }
   
+
+  module.exports.update = async function(req, res) {
+    if (req.user.id == req.params.id) {
+        try {
+            await User.findByIdAndUpdate(req.params.id, req.body);
+            return res.redirect('back');
+        } catch (err) {
+            // handle error
+        }
+    } else {
+        return res.status(401).send('Unauthorized');
+    }
+}
 // render the sign up page
 module.exports.signUp = function(req, res){
    if(req.isAuthenticated()){
