444386d9642a7fbf495397f2c728ee265bfed8c8
Pratham <Pratham.singla.762@gmail.com>
2023-06-01 02:17:57 +0530

    Using ajax for deleting post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,46 +1,42 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-module.exports.home = async function(req, res) {
-  try {
-    const posts = await Post.find({})
-      .populate('user')
-      .populate({
-        path: 'comments',
-        populate: {
-          path: 'user'
-        }
-      });
-    const users = await User.find({});
-    return res.render('home', {
-      title: "Codeial | Home",
-      posts: posts,
-      all_users: users
-    });
-  } catch (err) {
-    req.flash('err',err);
-  }
+
+
+
+module.exports.home = async function(req, res){
+
+    try{
+         // populate the user of each post
+        let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+    
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts,
+            all_users: users
+        });
+
+    }catch(err){
+        req.flash('err',err);
+    }
+   
 }
 
+// module.exports.actionName = function(req, res){}
 
-// module.exports.home = function(req, res){
-//     // console.log(req.cookies);
-//     // res.cookie('user_id', 25);
 
-//     // Post.find({}, function(err, posts){
-//     //     return res.render('home', {
-//     //         title: "Codeial | Home",
-//     //         posts:  posts
-//     //     });
-//     // });
+// using then
+// Post.find({}).populate('comments').then(function());
 
-//     // populate the user of each post
-//     Post.find({}).populate('user').exec(function(err, posts){
-//         return res.render('home', {
-//             title: "Codeial | Home",
-//             posts:  posts
-//         });
-//     })
+// let posts = Post.find({}).populate('comments').exec();
 
-// }
-
-// // module.exports.actionName = function(req, res){}
+// posts.then()
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,63 +1,64 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
-module.exports.create = async function(req, res) {
-    try {
-      let post = await Post.create({
-        content: req.body.content,
-        user: req.user._id
-      });
 
-      if(req.xhr){
-        return res.status(200).json({
-          data:{
-            post: post
-          },
-          message:"Post Created!"
+module.exports.create = async function(req, res){
+    try{
+        let post = await Post.create({
+            content: req.body.content,
+            user: req.user._id
         });
-      }
-      req.flash('success',"Post Created");
-    } catch (err) {
+        
+        if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
+
+        req.flash('success', 'Post published!');
+        return res.redirect('back');
+
+    }catch(err){
       req.flash('error','Error in creating Post');
+        return res.redirect('back');
     }
-    return res.redirect('back');
-  }
   
-// module.exports.destroy = function(req,res){
-//   Post.findById(req.params.id,function(err,post){
-// //.id means converting the object id into string
-//     if(post.user == req.user.id){
-//       post.remove();
-//       Comment.deleteMany({post:req.params.id},function(err){
-//         return res.redirect('back');
-//       });
-//     }
-//     else{
-//       return res.redirect('back');
-//     }
-//   });
-// }
+}
+
 
 module.exports.destroy = async function(req, res){
 
-  try{
-      let post = await Post.findById(req.params.id);
+    try{
+        let post = await Post.findById(req.params.id);
 
-      if (post.user == req.user.id){
-          post.remove();
+        if (post.user == req.user.id){
+           await Post.deleteOne({ _id: req.params.id })
 
-          await Comment.deleteMany({post: req.params.id});
+            await Comment.deleteMany({post: req.params.id});
 
-          req.flash('success', 'Post and associated comments deleted!');
 
-          return res.redirect('back');
-      }else{
-          req.flash('error', 'You cannot delete this post!');
-          return res.redirect('back');
-      }
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
 
-  }catch(err){
-      req.flash('error', err);
-      return res.redirect('back');
-  }
-  
+            req.flash('success', 'Post and associated comments deleted!');
+
+            return res.redirect('back');
+        }else{
+            req.flash('error', 'You cannot delete this post!');
+            return res.redirect('back');
+        }
+
+    }catch(err){
+        req.flash('error', err);
+        return res.redirect('back');
+    }
+    
 }
