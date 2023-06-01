d649c140bbafa970e73be4818b55c11abed6eeaa
Pratham <Pratham.singla.762@gmail.com>
2023-06-02 02:52:50 +0530

    some api practice and install passport jwt

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,44 @@
-module.exports.index = function(req,res){
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req,res){
+    let posts = await Post.find({})
+    .sort('-createdAt')
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    });
+   
     return res.json(200,{
         message:"List of posts",
-        posts:[]
+        posts:posts
     })
+}
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.params.id);
+           await Post.deleteOne({ _id: req.params.id })
+
+            await Comment.deleteMany({post: req.params.id});
+
+            return res.json(200,{
+                    message:"Post and associated coomment deleted successfully"
+            });
+        // }else{
+        //     req.flash('error', 'You cannot delete this post!');
+        //     return res.redirect('back');
+        // }
+
+    }
+    catch(err){
+        return res.json(500,{
+            message:"Internal Server Error"
+        });
+    }
+    
 }
